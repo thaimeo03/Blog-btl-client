c3e0bdcf4b74ed1bd10a3f76c68327a77e0169c4
thaimeo03 <thaitran15072003@gmail.com>
2024-06-01 16:50:52 +0700

    Update: account admin in login form

--- app/(auth)/_components/AuthForm.tsx
+++ app/(auth)/_components/AuthForm.tsx
@@ -62,6 +62,7 @@ export default function AuthForm() {
 
   return (
     <div className='min-h-screen flex flex-col items-center justify-center bg-gray-300'>
+      <div className='text-red-500'>Accout admin: thai@gmail.com 123456</div>
       <div className='flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md'>
         <div className='font-medium self-center text-xl sm:text-2xl uppercase text-gray-800'>
           {getInfoByRole({ registerInfo: 'Create New Account', loginInfo: 'Login To Your Account' })}
